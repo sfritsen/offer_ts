a793c5d7ef8db7874049035b217350454cf5d94d
sfritsen <sfritsen@gmail.com>
2022-12-10 23:17:02 -0700

    Got working again

--- dest/script.js
+++ dest/script.js
@@ -1,10 +1,11 @@
 "use strict";
+// For local web server dev testing
 // https://gist.github.com/jgravois/5e73b56fa7756fd00b89
 // Get each selector
-const form = document.querySelector("tool_form");
-const internet = document.querySelector("#internet");
-const tv = document.querySelector("#tv");
-const output = document.querySelector("#output");
+const form = document.getElementById("tool_form");
+const internet = document.getElementById("internet");
+// const tv = document.querySelector<HTMLSelectElement>("#tv")
+const output = document.getElementById("output");
 // Watch the form for changes and process them
 form === null || form === void 0 ? void 0 : form.addEventListener("change", processQuote);
 // Alt form listener used by button thats been removed
@@ -18,6 +19,9 @@ function processQuote() {
 function processInternet() {
     // if (internet?.value == "" || internet?.value == null) return
     if ((internet === null || internet === void 0 ? void 0 : internet.value) !== "" || (internet === null || internet === void 0 ? void 0 : internet.value) !== null) {
-        console.log(internet === null || internet === void 0 ? void 0 : internet.value);
+        // console.log(internet.value)
+        const intOutput = document.createElement("p");
+        intOutput.append(output.innerHTML += internet.value);
+        output.append += intOutput;
     }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,11 @@
+// For local web server dev testing
 // https://gist.github.com/jgravois/5e73b56fa7756fd00b89
 
 // Get each selector
-const form = document.querySelector<HTMLFormElement>("tool_form")
-const internet = document.querySelector<HTMLSelectElement>("#internet")
-const tv = document.querySelector<HTMLSelectElement>("#tv")
-const output = document.querySelector<HTMLElement>("#output")
-
+const form = document.getElementById("tool_form") as HTMLFormElement
+const internet = document.getElementById("internet") as HTMLSelectElement
+// const tv = document.querySelector<HTMLSelectElement>("#tv")
+const output: any = document.getElementById("output") as HTMLFormElement
 
 // Watch the form for changes and process them
 form?.addEventListener("change", processQuote)
@@ -23,6 +23,10 @@ function processQuote() {
 function processInternet() {
     // if (internet?.value == "" || internet?.value == null) return
     if (internet?.value !== "" || internet?.value !== null) {
-        console.log(internet?.value)
+        // console.log(internet.value)
+
+        const intOutput = document.createElement("p")
+        intOutput.append(output.innerHTML += internet.value)
+        output.append += intOutput
     }
 }
